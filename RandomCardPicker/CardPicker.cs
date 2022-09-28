76718f57f722760eb4856bb44c84dc2bae761616
mo4km <mo4km+it@protonmail.com>
2022-09-28 20:04:24 +0200

    Add project files.

---
+++ RandomCardPicker/CardPicker.cs
@@ -0,0 +1,29 @@
+﻿
+namespace RandomCardPicker
+{
+    public class CardPicker
+    {
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] deck = new string[52];
+            string[] numbers = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A" };
+            string[] drawnCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                drawnCards[i] = RandomValue() + RandomSuit();
+            }
+
+            return drawnCards;
+        }
+
+        private static string RandomSuit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string RandomValue()
+        {
+            
+        }
+    }
+}
---
+++ RandomCardPicker/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RandomCardPicker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+
+    }
+}
