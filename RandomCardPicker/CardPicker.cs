70d5dc86b1a310231fc894408437394f19ee5c02
mo4km <mo4km+it@protonmail.com>
2022-09-28 20:13:33 +0200

    added cardsuit enum and which is used by randomsuid method in cardpicker class

--- RandomCardPicker/CardPicker.cs
+++ RandomCardPicker/CardPicker.cs
@@ -3,8 +3,10 @@ namespace RandomCardPicker
 {
     public class CardPicker
     {
+        static Random random = new Random();
         public static string[] PickSomeCards(int numberOfCards)
         {
+            
             string[] deck = new string[52];
             string[] numbers = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A" };
             string[] drawnCards = new string[numberOfCards];
@@ -18,7 +20,8 @@ namespace RandomCardPicker
 
         private static string RandomSuit()
         {
-            throw new NotImplementedException();
+            CardSuit suit = (CardSuit)random.Next(1, 5);
+            return suit.ToString();            
         }
 
         private static string RandomValue()
---
+++ RandomCardPicker/CardSuit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCardPicker
+{
+    public enum CardSuit
+    {
+        kier = 1,
+        trefl,
+        pik,
+        karo
+
+    }
+}
