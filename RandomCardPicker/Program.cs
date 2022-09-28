a0ed17743f27c2f1a4430f0e84e5d945a919d93a
mo4km <mo4km+it@protonmail.com>
2022-09-28 20:20:51 +0200

    added pickSomeCards method in main class

--- RandomCardPicker/Program.cs
+++ RandomCardPicker/Program.cs
@@ -5,7 +5,12 @@ namespace RandomCardPicker
     {
         static void Main(string[] args)
         {
-            
+            string[] cards = new string[];
+            cards = CardPicker.PickSomeCards(2);
+            foreach (var item in cards)
+            {
+                Console.WriteLine(item);
+            }
             
         }
 
