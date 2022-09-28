3a61f095398be5e2376e098cd43181d3d3545d75
mo4km <mo4km+it@protonmail.com>
2022-09-28 20:23:18 +0200

    changed the way to pass number of drawn random cards to user input

--- RandomCardPicker/Program.cs
+++ RandomCardPicker/Program.cs
@@ -5,8 +5,10 @@ namespace RandomCardPicker
     {
         static void Main(string[] args)
         {
-            string[] cards = new string[];
-            cards = CardPicker.PickSomeCards(2);
+            Console.WriteLine("How many random cards would you like to draw: ");
+            Int32.TryParse(Console.ReadLine(), out int nCards);
+            string[] cards = new string[nCards];
+            cards = CardPicker.PickSomeCards(nCards);
             foreach (var item in cards)
             {
                 Console.WriteLine(item);
